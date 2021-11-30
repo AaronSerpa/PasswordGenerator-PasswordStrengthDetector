969244d9eadee2baaf0b651a27913f4adb3afa84
Aaron Serpa <60374482+AaronSerpa@users.noreply.github.com>
2021-11-29 17:30:11 -0800

    password generation added

--- src/Controller.java
+++ src/Controller.java
@@ -1,25 +1,136 @@
+import java.util.Random;
+import java.lang.*;
 /**
- * links the Model and View
+ * Houses the means of generating passwords
+ * takes two strings as inputs that contain info from the user
+ * these two strings are then used to create a password
  * @author 
  *
  */
-public class Controller 
+public class Password_Generator 
 {
-	Model model;
-	View view;
-	public Controller( Model m, View v)
+	String personal1; 
+	String personal2;
+
+	public Password_Generator(String p1, String p2)
 	{
-		model = m;
-		view = v;
+		personal1 = p1;
+		personal2 = p2;
+
 	}
-	
-	/*
-	 * Generates password with a Password_Generator object
-	 * then passes it to the model to test it
-	 * then updates the view with the test reults
+
+	/**
+	 * uses the two instance variables given and mutates them into a password
+	 * @return
 	 */
-	public void GetPassword()
+	public String GeneratePassword()
 	{
-		
+		Random rand = new Random();
+
+		String temp = "";
+		//generate random number
+		int random = rand.nextInt(3)+1;
+		//determine what goes in the middle of the two words
+		//1-> nothing
+		if(random == 1)
+		{
+			temp = personal1+personal2;
+		}
+		//2-> underscore
+		else if (random == 2)
+		{
+			temp = personal1+"_"+personal2;
+		}
+		//3-> dash
+		else if (random == 3)
+		{
+			temp = personal1+"-"+personal2;
+		}
+		StringBuilder pw = new StringBuilder(temp);
+		//loop through the words
+		for(int i = 0; i<temp.length(); i++)
+		{
+			//generate random number
+			random = rand.nextInt(2)+1;
+			//1-> uppercase
+			if(random == 1)
+			{
+				pw.setCharAt(i, Character.toUpperCase(temp.charAt(i)));
+			}
+			//2-> lowercase
+			else if (random == 2)
+			{
+				pw.setCharAt(i, Character.toLowerCase(temp.charAt(i)));
+			}
+		}
+		//loop through the words
+		for(int i = 0; i<temp.length(); i++)
+		{
+			//generate random number
+			random = rand.nextInt(2)+1;
+			//1-> 'A' or 'a' becomes '@' or 'S' or 's' becomes '$' and so on
+			if(random == 1)
+			{
+				if(Character.toLowerCase(temp.charAt(i)) == 'a')
+				{
+					pw.setCharAt(i, '@');
+				}
+				else if(Character.toLowerCase(temp.charAt(i)) == 's')
+				{
+					pw.setCharAt(i, '$');
+				}
+				else if(Character.toLowerCase(temp.charAt(i)) == 'o')
+				{
+					pw.setCharAt(i, '0');
+				}
+				else if(Character.toLowerCase(temp.charAt(i)) == 'b')
+				{
+					pw.setCharAt(i, '8');
+				}
+				else if(Character.toLowerCase(temp.charAt(i)) == 'c')
+				{
+					pw.setCharAt(i, '(');
+				}
+				else if(Character.toLowerCase(temp.charAt(i)) == 'i' || Character.toLowerCase(temp.charAt(i)) == 'l')
+				{
+					pw.setCharAt(i, '1');
+				}
+			}
+		}
+		temp = pw.toString();
+		//generate random number
+		random = rand.nextInt(2)+1;
+		//1-> add some special characters to start
+		if(random == 1)
+		{
+			//generate random number
+			String chars= "";
+			random = rand.nextInt(11)+33; // ascii values of different symbols
+			chars = chars+(char)random;
+			random = rand.nextInt(11)+33; // ascii values of different symbols
+			chars = chars+(char)random;
+			random = rand.nextInt(11)+33; // ascii values of different symbols
+			chars = chars+(char)random;
+			temp = chars+temp;
+		}
+
+		//2-> add some special characters to end
+		else if (random == 2)
+		{
+			//generate random number
+			String chars= "";
+			random = rand.nextInt(11)+33; // ascii values of different symbols
+			chars = chars+(char)random;
+			random = rand.nextInt(11)+33; // ascii values of different symbols
+			chars = chars+(char)random;
+			random = rand.nextInt(11)+33; // ascii values of different symbols
+			chars = chars+(char)random;
+			temp = temp+chars;
+		}
+		//generate random number
+		random = rand.nextInt(20)+1; 
+		//add random number to end of password
+		temp = temp+random;
+		return temp;
 	}
 }
